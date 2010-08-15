bd6929c1572bd2248a220c073e4b81227ec571a7
Takayama Fumihiko <tekezo@pqrs.org>
2010-08-15 14:34:31 +0900

    update mkae-package.sh

--- make-package.sh
+++ make-package.sh
@@ -5,3 +5,4 @@ make clean all
 version=`ruby -e 'data = $stdin.read; /<key>CFBundleVersion<\/key>\s*<string>(.+?)<\/string>/m =~ data; print $1, "\n"' < build/Release/DrasticInputSourceStatus.app/Contents/Info.plist`
 (cd build/Release; zip -r DrasticInputSourceStatus-$version.app.zip DrasticInputSourceStatus.app)
 mv build/Release/DrasticInputSourceStatus-$version.app.zip .
+chmod 644 DrasticInputSourceStatus-$version.app.zip
