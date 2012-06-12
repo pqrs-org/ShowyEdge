c696ef59b503d32cb99521856617f49fdb661740
Takayama Fumihiko <tekezo@pqrs.org>
2012-06-12 11:09:33 +0900

    change package archive format to dmg from zip

--- make-package.sh
+++ make-package.sh
@@ -3,6 +3,10 @@
 make clean all
 
 version=`ruby -e 'data = $stdin.read; /<key>CFBundleVersion<\/key>\s*<string>(.+?)<\/string>/m =~ data; print $1, "\n"' < build/Release/DrasticInputSourceStatus.app/Contents/Info.plist`
-(cd build/Release; zip -r DrasticInputSourceStatus-$version.app.zip DrasticInputSourceStatus.app)
-mv build/Release/DrasticInputSourceStatus-$version.app.zip .
-chmod 644 DrasticInputSourceStatus-$version.app.zip
+
+# --------------------------------------------------
+echo "Making dmg..."
+
+rm -f DrasticInputSourceStatus-$version.dmg
+hdiutil create -nospotlight DrasticInputSourceStatus-$version.dmg -srcfolder build/Release/DrasticInputSourceStatus.app
+chmod 644 DrasticInputSourceStatus-$version.dmg
