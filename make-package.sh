df2db7af699c35897a1c5710faaba19631418fb4
Takayama Fumihiko <tekezo@pqrs.org>
2013-07-15 22:34:19 +0900

    set PATH

--- make-package.sh
+++ make-package.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+PATH=/bin:/sbin:/usr/bin:/usr/sbin; export PATH
+
 make clean all
 
 version=`ruby -e 'data = $stdin.read; /<key>CFBundleVersion<\/key>\s*<string>(.+?)<\/string>/m =~ data; print $1, "\n"' < build/Release/ShowyEdge.app/Contents/Info.plist`
