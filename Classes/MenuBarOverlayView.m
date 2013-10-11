71e76fde27b4dc568eb5b7b317445fc57535bd57
Takayama Fumihiko <tekezo@pqrs.org>
2013-10-11 12:08:50 +0900

    change margin

--- Classes/MenuBarOverlayView.m
+++ Classes/MenuBarOverlayView.m
@@ -22,11 +22,9 @@
   NSRect fullrect = [self frame];
 
   CGFloat width = fullrect.size.width / 3;
+  CGFloat margin = 10;
 
-  [[NSColor redColor] set];
-  NSRectFill(NSMakeRect(0, 0, 0, fullrect.size.height));
-
-  // To avoid a gap between each colors, we specify "width * 1.2" as color bar width.
+  // To avoid a gap between each colors, we specify "width + margin" as color bar width.
   //
   //
   // In the case that color configuration is "red, green, blue":
@@ -40,7 +38,7 @@
   //   |------------------| |------------------| |--------------------|
   //    Red                  Green                Blue
   //
-  // Therefore, we need to draw color bars by width*1.2 x fullrect.size.height.
+  // Therefore, we need to draw color bars by width + margin x fullrect.size.height.
   //
   //   |-----------------------|
   //                        |-----------------------|
@@ -48,13 +46,13 @@
   //    Red                  Green                Blue
 
   [self.color0 set];
-  NSRectFill(NSMakeRect(width * 0, 0, width * 1.2, fullrect.size.height));
+  NSRectFill(NSMakeRect(width * 0, 0, width + margin, fullrect.size.height));
 
   [self.color1 set];
-  NSRectFill(NSMakeRect(width * 1, 0, width * 1.2, fullrect.size.height));
+  NSRectFill(NSMakeRect(width * 1, 0, width + margin, fullrect.size.height));
 
   [self.color2 set];
-  NSRectFill(NSMakeRect(width * 2, 0, width * 1.2, fullrect.size.height));
+  NSRectFill(NSMakeRect(width * 2, 0, width + margin, fullrect.size.height));
 }
 
 - (void) setColor:(NSColor*)c0 c1:(NSColor*)c1 c2:(NSColor*)c2
