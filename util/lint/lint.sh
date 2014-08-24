808c4c6364ddb702e374e36482fd00b5ea06d442
Takayama Fumihiko <tekezo@pqrs.org>
2014-08-24 11:50:12 +0900

    ignore Pods from lint,uncrustify

--- util/lint/lint.sh
+++ util/lint/lint.sh
@@ -4,7 +4,7 @@ basedir=`dirname $0`
 
 ############################################################
 # xcodeproj
-for f in `find $basedir/../../* -name 'project.pbxproj'`; do
+for f in `find $basedir/../../* -name 'project.pbxproj' ! -ipath '*/Pods/*'`; do
     echo "Check $f"
 
     plutil -convert json -o - "$f" | "$basedir/xcodeproj.rb"  || exit 1
