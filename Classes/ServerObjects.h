7a46758f5c6e88336fb0427c70e5c9ad51f97faf
Takayama Fumihiko <tekezo@pqrs.org>
2016-03-29 01:29:15 +0900

    add WorkSpaceData into ServerObjects

--- Classes/ServerObjects.h
+++ Classes/ServerObjects.h
@@ -4,10 +4,12 @@
 
 @class PreferencesManager;
 @class Updater;
+@class WorkSpaceData;
 
 @interface ServerObjects : NSObject
 
 @property(weak) IBOutlet PreferencesManager* preferencesManager;
 @property(weak) IBOutlet Updater* updater;
+@property(weak) IBOutlet WorkSpaceData* workSpaceData;
 
 @end
