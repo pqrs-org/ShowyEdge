3ddeead505e500d283db5fdd09f3e43aca8ef784
Takayama Fumihiko <tekezo@pqrs.org>
2016-03-29 01:30:27 +0900

    add observer_kCurrentInputSourceIDChangedNotification

--- Classes/Preferences/PreferencesWindowController.m
+++ Classes/Preferences/PreferencesWindowController.m
@@ -1,23 +1,43 @@
+#import "LanguageColorTableViewController.h"
+#import "NotificationKeys.h"
 #import "PreferencesKeys.h"
 #import "PreferencesWindowController.h"
 #import "Relauncher.h"
 #import "ServerController.h"
 #import "ServerObjects.h"
 #import "Updater.h"
+#import "WorkSpaceData.h"
 
 @interface PreferencesWindowController ()
 
+@property(weak) IBOutlet LanguageColorTableViewController* languageColorTableViewController;
+@property(weak) IBOutlet NSTextField* currentInputSourceID;
 @property(weak) IBOutlet NSTextField* versionText;
 
 @end
 
 @implementation PreferencesWindowController
 
+- (void)observer_kCurrentInputSourceIDChangedNotification:(NSNotification*)notification {
+  dispatch_async(dispatch_get_main_queue(), ^{
+    if ([self.serverObjects.workSpaceData.currentInputSourceID length] > 0) {
+      [self.currentInputSourceID setStringValue:self.serverObjects.workSpaceData.currentInputSourceID];
+    }
+  });
+}
+
 - (instancetype)initWithServerObjects:(NSString*)windowNibName serverObjects:(ServerObjects*)serverObjects {
   self = [super initWithWindowNibName:windowNibName];
 
   if (self) {
     self.serverObjects = serverObjects;
+
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(observer_kCurrentInputSourceIDChangedNotification:)
+                                                 name:kCurrentInputSourceIDChangedNotification
+                                               object:nil];
+
+    [self observer_kCurrentInputSourceIDChangedNotification:nil];
 
     // Show icon in Dock only when Preferences has been opened.
     if ([[NSUserDefaults standardUserDefaults] boolForKey:kShowIconInDock]) {
@@ -27,6 +47,10 @@
   }
 
   return self;
+}
+
+- (void)dealloc {
+  [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 - (void)drawVersion {
@@ -41,6 +65,16 @@
 - (void)show {
   [self.window makeKeyAndOrderFront:self];
   [NSApp activateIgnoringOtherApps:YES];
+}
+
+- (IBAction)add:(id)sender {
+  if ([self.serverObjects.workSpaceData.currentInputSourceID length] > 0) {
+    [self.languageColorTableViewController add:self.serverObjects.workSpaceData.currentInputSourceID];
+  }
+}
+
+- (IBAction)remove:(id)sender {
+  [self.languageColorTableViewController remove];
 }
 
 - (IBAction)quit:(id)sender {
