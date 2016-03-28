8b5226e6ee035440414d5a0f7fc5db55214a7e91
Takayama Fumihiko <tekezo@pqrs.org>
2016-03-29 01:17:06 +0900

    add kCurrentInputSourceIDChangedNotification

--- Classes/NotificationKeys.h
+++ Classes/NotificationKeys.h
@@ -1,4 +1,5 @@
 // -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*-
 
+#define kCurrentInputSourceIDChangedNotification @"kCurrentInputSourceIDChangedNotification"
 #define kIndicatorColorChangedNotification @"IndicatorColorChangedNotification"
 #define kIndicatorHeightChangedNotification @"IndicatorHeightChangedNotification"
