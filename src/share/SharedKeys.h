2ec37f177a43fe018824ae12d04943858327e5bd
Takayama Fumihiko <tekezo@pqrs.org>
2016-04-03 22:00:42 +0900

    add SharedKeys.h

---
+++ src/share/SharedKeys.h
@@ -0,0 +1,9 @@
+// -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*-
+
+// ------------------------------------------------------------
+// NSDistributedNotificationCenter
+#define kShowyEdgeServerDidLaunchNotification @"org.pqrs.ShowyEdge.notification.serverDidLaunch"
+
+// ------------------------------------------------------------
+// NSConnection
+#define kShowyEdgeConnectionName @"org.pqrs.ShowyEdge.server"
