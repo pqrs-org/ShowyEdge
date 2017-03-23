317a9e69559444bbf79789622663e1bc10abb6db
Takayama Fumihiko <tekezo@pqrs.org>
2017-03-23 23:48:29 +0900

    remove GEM_HOME from PATH

--- make-package.sh
+++ make-package.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# set $GEM_HOME/bin/ for CocoaPods.
-PATH="/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:$GEM_HOME/bin"; export PATH
+PATH="/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin"; export PATH
 
 version=$(cat version)
 
