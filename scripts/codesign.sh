069a4f232d1bdd99492989659fbc9ac1cd0ecf30
Takayama Fumihiko <tekezo@pqrs.org>
2019-10-05 23:09:48 +0900

    update scripts/codesign.sh

--- scripts/codesign.sh
+++ scripts/codesign.sh
@@ -1,48 +1,66 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-CODESIGN_IDENTITY='8D660191481C98F5C56630847A6C39D95C166F22'
+set -u # forbid undefined variables
+set -e # forbid command failure
 
-# ------------------------------------------------------------
-PATH=/bin:/sbin:/usr/bin:/usr/sbin
+readonly CODESIGN_IDENTITY='8D660191481C98F5C56630847A6C39D95C166F22'
+readonly PATH=/bin:/sbin:/usr/bin:/usr/sbin
 export PATH
 
-if [ ! -e "$1" ]; then
-    echo "[ERROR] Invalid argument: '$1'"
-    exit 1
-fi
+err() {
+    echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
+}
 
-# ------------------------------------------------------------
-# sign
-cd "$1"
-find * -name '*.app' -or -name '*.signed.kext' -or -path '*/bin/*' | sort -r | while read f; do
-    #
-    # output message
-    #
-
-    echo -ne '\033[33;40m'
-    echo "code sign $f"
-    echo -ne '\033[0m'
+main() {
+    if [ ! -e "$1" ]; then
+        err "Invalid argument: '$1'"
+        exit 1
+    fi
 
     #
-    # codesign
+    # Sign with codesign
     #
 
-    echo -ne '\033[31;40m'
+    cd "$1"
+    find * -name '*.app' -or -path '*/bin/*' | sort -r | while read f; do
+        #
+        # output message
+        #
 
-    codesign \
-        --force \
-        --deep \
-        --options runtime \
-        --sign "$CODESIGN_IDENTITY" \
-        "$f" 2>&1 |
-        grep -v ': replacing existing signature'
+        echo -ne '\033[33;40m'
+        echo "code sign $f"
+        echo -ne '\033[0m'
 
-    echo -ne '\033[0m'
-done
+        #
+        # codesign
+        #
 
-# verify
-find * -name '*.app' -or -name '*.signed.kext' -or -path '*/bin/*' | sort -r | while read f; do
-    echo -ne '\033[31;40m'
-    codesign --verify --deep "$f"
-    echo -ne '\033[0m'
-done
+        echo -ne '\033[31;40m'
+
+        set +e # allow command failure
+
+        codesign \
+            --force \
+            --deep \
+            --options runtime \
+            --sign "$CODESIGN_IDENTITY" \
+            "$f" 2>&1 |
+            grep -v ': replacing existing signature'
+
+        set -e # forbid command failure
+
+        echo -ne '\033[0m'
+    done
+
+    #
+    # Verify codesign
+    #
+
+    find * -name '*.app' -or -path '*/bin/*' | sort -r | while read f; do
+        echo -ne '\033[31;40m'
+        codesign --verify --deep "$f"
+        echo -ne '\033[0m'
+    done
+}
+
+main "$1"
