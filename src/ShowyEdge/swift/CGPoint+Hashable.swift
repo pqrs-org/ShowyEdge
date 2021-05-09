1fd4cc4be4bc9751ca292406b7a027f0e5c89758
Takayama Fumihiko <tekezo@pqrs.org>
2021-05-09 14:22:17 +0900

    Update indent size

--- src/ShowyEdge/swift/CGPoint+Hashable.swift
+++ src/ShowyEdge/swift/CGPoint+Hashable.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 extension CGPoint: Hashable {
-  public func hash(into hasher: inout Hasher) {
-    hasher.combine(x)
-    hasher.combine(y)
-  }
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(x)
+        hasher.combine(y)
+    }
 }
