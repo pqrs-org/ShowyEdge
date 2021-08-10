b7e9431ebfc274b4cb8c281459b0e6c9ef850b25
Takayama Fumihiko <tekezo@pqrs.org>
2021-08-11 00:26:38 +0900

    Add Open at login, Show icon in menu bar" to PreferencesBasicView

--- src/ShowyEdge/swift/Views/PreferencesBasicView.swift
+++ src/ShowyEdge/swift/Views/PreferencesBasicView.swift
@@ -4,9 +4,25 @@ struct PreferencesBasicView: View {
     @ObservedObject var userSettings = UserSettings.shared
 
     var body: some View {
-        VStack {
-            Text("Basic View")
-        }
+        VStack(alignment: .leading, spacing: 25.0) {
+            GroupBox(label: Text("Basic")) {
+                VStack(alignment: .leading, spacing: 10.0) {
+                    Toggle(isOn: $userSettings.openAtLogin) {
+                        Text("Open at login)")
+                        Spacer()
+                    }
+
+                    Toggle(isOn: $userSettings.showMenu) {
+                        Text("Show icon in menu bar")
+                        Spacer()
+                    }
+                }
+
+                .padding()
+            }
+
+            Spacer()
+        }.padding()
     }
 }
 
