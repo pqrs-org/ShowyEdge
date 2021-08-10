8528abf5047c38cb5b77583b6bd6b98c5598e8fa
Takayama Fumihiko <tekezo@pqrs.org>
2021-08-10 23:44:22 +0900

    Add CustomFrameUnitPicker

---
+++ src/ShowyEdge/swift/Views/CustomFrameUnitPicker.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct CustomFrameUnitPicker: View {
+    @Binding var value: Int
+
+    var body: some View {
+        Picker(selection: $value, label: Text("")) {
+             Text("pt").tag(0)
+             Text("%").tag(1)
+        }.frame(width: 60.0)
+    }
+}
