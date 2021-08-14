f1ca303a36fe4bde039e4bdfa4aa4d5cb4dae378
Takayama Fumihiko <tekezo@pqrs.org>
2021-08-14 09:28:15 +0900

    Add IndicatorColors

---
+++ src/ShowyEdge/swift/IndicatorColors.swift
@@ -0,0 +1,7 @@
+import SwiftUI
+
+class IndicatorColors: ObservableObject {
+    static let shared = IndicatorColors()
+
+    @Published var colors: (Color, Color, Color) = (.clear, .clear, .clear)
+}
