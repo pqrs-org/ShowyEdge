a00c2e2d09e820c612638d0112a9152023312983
Takayama Fumihiko <tekezo@pqrs.org>
2021-08-14 20:12:49 +0900

    Set explicit value to LanguageColor.id

--- src/ShowyEdge/swift/Models/LanguageColor.swift
+++ src/ShowyEdge/swift/Models/LanguageColor.swift
@@ -1,10 +1,12 @@
 import SwiftUI
 
 class LanguageColor: Identifiable {
+    var id: String
     var inputSourceID: String
     var colors: (Color, Color, Color)
 
     init(_ inputSourceID: String, _ colors: (Color, Color, Color)) {
+        id = inputSourceID
         self.inputSourceID = inputSourceID
         self.colors = colors
     }
