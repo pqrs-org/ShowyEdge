0f0eaff93d840f4808c66b41f63eeeac4f9d6583
Takayama Fumihiko <tekezo@pqrs.org>
2022-01-29 23:52:53 +0900

    Apply swift-format

--- src/ShowyEdge/swift/Relauncher.swift
+++ src/ShowyEdge/swift/Relauncher.swift
@@ -2,16 +2,16 @@ import AppKit
 import Foundation
 
 struct Relauncher {
-    static func relaunch() {
-        do {
-            print("relaunch")
+  static func relaunch() {
+    do {
+      print("relaunch")
 
-            let process = Process()
-            process.executableURL = Bundle.main.executableURL
-            try process.run()
-            NSApplication.shared.terminate(self)
-        } catch {
-            print("Process.run error: \(error)")
-        }
+      let process = Process()
+      process.executableURL = Bundle.main.executableURL
+      try process.run()
+      NSApplication.shared.terminate(self)
+    } catch {
+      print("Process.run error: \(error)")
     }
+  }
 }
