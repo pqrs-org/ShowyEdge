44a49136f106b2e4ddf3c7c14bc591683eb40fb9
Takayama Fumihiko <tekezo@pqrs.org>
2022-01-29 20:47:02 +0900

    Update scripts/notarize-app.sh (xcrun altool -> xcrun notarytool)

--- scripts/notarize-app.sh
+++ scripts/notarize-app.sh
@@ -6,16 +6,13 @@ for f in $(dirname $0)/../ShowyEdge-*.dmg; do
     if [ -f $f ]; then
         basename $f
 
-        if xcrun altool --notarize-app \
-            -t osx \
-            -f $f \
-            --primary-bundle-id 'org.pqrs.ShowyEdge' \
-            -u 'tekezo@pqrs.org' \
-            -p '@keychain:pqrs.org-notarize-app'; then
-            echo
-            echo "Run the following command after this notarization is succeeded."
-            echo
-            echo "xcrun stapler staple $(basename $f)"
+        if xcrun notarytool \
+            submit $(basename $f) \
+            --keychain-profile "pqrs.org notarization" \
+            --wait \
+            ; then
+            xcrun stapler staple $(basename $f)
+            say completed
         fi
     fi
 done
