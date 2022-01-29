d7c76fa562ad5ed6bbb4cf02f6cf547c08558025
Takayama Fumihiko <tekezo@pqrs.org>
2022-01-29 13:10:32 +0900

    Remove unnecessary VStack

--- src/ShowyEdge/swift/Views/PreferencesView.swift
+++ src/ShowyEdge/swift/Views/PreferencesView.swift
@@ -8,61 +8,57 @@ struct PreferencesView: View {
   var body: some View {
     let padding = 6.0
 
-    VStack {
-      NavigationView {
-        VStack(alignment: .leading, spacing: 0) {
-          List {
-            NavigationLink(
-              destination: PreferencesBasicView(),
-              tag: "Basic",
-              selection: $selection
-            ) {
-              Label("Basic", systemImage: "gearshape")
-            }
-            .padding(padding)
-
-            NavigationLink(
-              destination: PreferencesIndicatorView(),
-              tag: "Indicator",
-              selection: $selection
-            ) {
-              Label("Indicator", systemImage: "wrench")
-            }
-            .padding(padding)
-
-            NavigationLink(
-              destination: PreferencesCustomFrameView(),
-              tag: "Custom Frame",
-              selection: $selection
-            ) {
-              Label("Custom Frame", systemImage: "hammer")
-            }
-            .padding(padding)
-
-            NavigationLink(
-              destination: PreferencesMiscView(),
-              tag: "Misc",
-              selection: $selection
-            ) {
-              Label("Misc", systemImage: "cube")
-            }
-            .padding(padding)
-
-            Divider()
-
-            NavigationLink(
-              destination: PreferencesActionView(),
-              tag: "Action",
-              selection: $selection
-            ) {
-              Label("Quit, Restart", systemImage: "bolt.circle")
-            }
-            .padding(padding)
-          }
-          .listStyle(SidebarListStyle())
-          .frame(width: 200)
+    NavigationView {
+      List {
+        NavigationLink(
+          destination: PreferencesBasicView(),
+          tag: "Basic",
+          selection: $selection
+        ) {
+          Label("Basic", systemImage: "gearshape")
         }
+        .padding(padding)
+
+        NavigationLink(
+          destination: PreferencesIndicatorView(),
+          tag: "Indicator",
+          selection: $selection
+        ) {
+          Label("Indicator", systemImage: "wrench")
+        }
+        .padding(padding)
+
+        NavigationLink(
+          destination: PreferencesCustomFrameView(),
+          tag: "Custom Frame",
+          selection: $selection
+        ) {
+          Label("Custom Frame", systemImage: "hammer")
+        }
+        .padding(padding)
+
+        NavigationLink(
+          destination: PreferencesMiscView(),
+          tag: "Misc",
+          selection: $selection
+        ) {
+          Label("Misc", systemImage: "cube")
+        }
+        .padding(padding)
+
+        Divider()
+
+        NavigationLink(
+          destination: PreferencesActionView(),
+          tag: "Action",
+          selection: $selection
+        ) {
+          Label("Quit, Restart", systemImage: "bolt.circle")
+        }
+        .padding(padding)
       }
+      .listStyle(SidebarListStyle())
+      .frame(width: 200)
     }.frame(width: 900, height: 550)
   }
 }
