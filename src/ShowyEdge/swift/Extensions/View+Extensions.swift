b5d962e0b89546d7d9d2e11986b416605074c583
Takayama Fumihiko <tekezo@pqrs.org>
2025-01-19 09:12:27 +0900

    Remove unused files

--- src/ShowyEdge/swift/Extensions/View+Extensions.swift
+++ src/ShowyEdge/swift/Extensions/View+Extensions.swift
@@ -1,12 +1,6 @@
 import SwiftUI
 
 extension View {
-  func sidebarButtonLabelStyle() -> some View {
-    self
-      .padding(10.0)
-      .contentShape(Rectangle())
-  }
-
   // https://gist.github.com/importRyan/c668904b0c5442b80b6f38a980595031
   func whenHovered(_ mouseIsInside: @escaping (Bool) -> Void) -> some View {
     modifier(MouseInsideModifier(mouseIsInside))
