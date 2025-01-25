fb68904a26a5b7700c7e76e893d091cfe008d536
Takayama Fumihiko <tekezo@pqrs.org>
2025-01-25 10:54:30 +0900

    Remove windowPositionUpdateNeededNotification

--- src/ShowyEdge/swift/Notifications.swift
+++ src/ShowyEdge/swift/Notifications.swift
@@ -1,11 +1,3 @@
 import Foundation
 
-let windowPositionUpdateNeededNotification = NSNotification.Name("windowPositionUpdateNeeded")
 let openSettingsNotification = NSNotification.Name("openSettings")
-
-func postWindowPositionUpdateNeededNotification() {
-  NotificationCenter.default.post(
-    name: windowPositionUpdateNeededNotification,
-    object: nil,
-    userInfo: nil)
-}
