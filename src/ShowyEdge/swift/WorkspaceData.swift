cf44db860f635d344cc7d3e40d7c5319830aa3a3
Takayama Fumihiko <tekezo@pqrs.org>
2025-01-25 23:26:57 +0900

    Add WorkspaceData.inputSourceNames

--- src/ShowyEdge/swift/WorkspaceData.swift
+++ src/ShowyEdge/swift/WorkspaceData.swift
@@ -7,11 +7,20 @@ public class WorkspaceData: NSObject, ObservableObject {
   public static let shared = WorkspaceData()
   public static let currentInputSourceChanged = Notification.Name("currentInputSourceChanged")
 
+  @Published var inputSourceNames: [String: String] = [:]
   @Published var currentInputSourceID: String = ""
   @Published var currentInputModeID: String = ""
 
   public func start() {
     stop()
+
+    DistributedNotificationCenter.default().addObserver(
+      self,
+      selector: #selector(enabledKeyboardInputSourcesChanged),
+      name: kTISNotifyEnabledKeyboardInputSourcesChanged as NSNotification.Name?,
+      object: nil,
+      suspensionBehavior: .deliverImmediately
+    )
 
     DistributedNotificationCenter.default().addObserver(
       self,
@@ -21,11 +30,45 @@ public class WorkspaceData: NSObject, ObservableObject {
       suspensionBehavior: .deliverImmediately
     )
 
+    enabledKeyboardInputSourcesChanged()
     selectedKeyboardInputSourceChanged()
   }
 
   public func stop() {
     DistributedNotificationCenter.default.removeObserver(self)
+  }
+
+  @MainActor
+  public func getInputSourceLocalizedName(inputSourceID: String) -> String {
+    if let name = inputSourceNames[inputSourceID] {
+      return name
+    }
+    return inputSourceID
+  }
+
+  @objc
+  private func enabledKeyboardInputSourcesChanged() {
+    Task { @MainActor in
+      guard
+        let inputSourceList = TISCreateInputSourceList(nil, false)?.takeUnretainedValue()
+          as? [TISInputSource]
+      else { return }
+
+      var newInputSourceNames: [String: String] = [:]
+
+      for inputSource in inputSourceList {
+        if let id = TISGetInputSourceProperty(inputSource, kTISPropertyInputSourceID),
+          let name = TISGetInputSourceProperty(inputSource, kTISPropertyLocalizedName)
+        {
+          let inputSourceID = String(unsafeBitCast(id, to: CFString.self))
+          let localizedName = String(unsafeBitCast(name, to: CFString.self))
+
+          newInputSourceNames[inputSourceID] = localizedName
+        }
+      }
+
+      inputSourceNames = newInputSourceNames
+    }
   }
 
   @objc
