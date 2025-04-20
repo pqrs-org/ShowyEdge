1bc157905bb0b960ef50e371bef1559daf447caf
Takayama Fumihiko <tekezo@pqrs.org>
2025-04-21 08:26:08 +0900

    Improve UI: SettingsFollowActiveWindowView

--- src/ShowyEdge/swift/Views/Settings/SettingsFollowActiveWindowView.swift
+++ src/ShowyEdge/swift/Views/Settings/SettingsFollowActiveWindowView.swift
@@ -12,55 +12,56 @@ struct SettingsFollowActiveWindowView: View {
           }
           .switchToggleStyle()
 
-          GroupBox(label: Text("Options")) {
-            VStack(alignment: .leading, spacing: 10.0) {
-              VStack(alignment: .leading, spacing: 0.0) {
-                Toggle(isOn: $userSettings.followFinderActiveWindow) {
-                  Text("Follow Finder window (Default: off)")
-                }
-                .switchToggleStyle()
+          if userSettings.followActiveWindow {
+            GroupBox(label: Text("Options")) {
+              VStack(alignment: .leading, spacing: 10.0) {
+                VStack(alignment: .leading, spacing: 0.0) {
+                  Toggle(isOn: $userSettings.followFinderActiveWindow) {
+                    Text("Follow Finder window (Default: off)")
+                  }
+                  .switchToggleStyle()
 
-                Text(
-                  "Enabling this setting may cause the indicator position to become incorrect when the desktop is focused."
-                )
-                .font(.caption)
-              }
-            }
-            .padding()
-            .frame(maxWidth: .infinity, alignment: .leading)
-          }
-
-          GroupBox(label: Text("Options")) {
-            VStack(alignment: .leading, spacing: 10.0) {
-              Grid(alignment: .leadingFirstTextBaseline) {
-                GridRow {
-                  Text("The minimum width of a window to be followed:")
-                    .gridColumnAlignment(.trailing)
-
-                  DoubleTextField(
-                    value: $userSettings.minWindowWidthToFollowActiveWindow,
-                    range: 0...10000,
-                    step: 10,
-                    maximumFractionDigits: 1,
-                    width: 50)
-                }
-
-                GridRow {
-                  Text("The minimum height of a window to be followed:")
-
-                  DoubleTextField(
-                    value: $userSettings.minWindowHeightToFollowActiveWindow,
-                    range: 0...10000,
-                    step: 10,
-                    maximumFractionDigits: 1,
-                    width: 50)
+                  Text(
+                    "Enabling this setting may cause the indicator position to become incorrect when the desktop is focused."
+                  )
+                  .font(.caption)
                 }
               }
+              .padding()
+              .frame(maxWidth: .infinity, alignment: .leading)
             }
-            .padding()
-            .frame(maxWidth: .infinity, alignment: .leading)
+
+            GroupBox(label: Text("Minimum size of a window to be followed")) {
+              VStack(alignment: .leading, spacing: 10.0) {
+                Grid(alignment: .leadingFirstTextBaseline) {
+                  GridRow {
+                    Text("Width:")
+                      .gridColumnAlignment(.trailing)
+
+                    DoubleTextField(
+                      value: $userSettings.minWindowWidthToFollowActiveWindow,
+                      range: 0...10000,
+                      step: 10,
+                      maximumFractionDigits: 1,
+                      width: 50)
+                  }
+
+                  GridRow {
+                    Text("Height:")
+
+                    DoubleTextField(
+                      value: $userSettings.minWindowHeightToFollowActiveWindow,
+                      range: 0...10000,
+                      step: 10,
+                      maximumFractionDigits: 1,
+                      width: 50)
+                  }
+                }
+              }
+              .padding()
+              .frame(maxWidth: .infinity, alignment: .leading)
+            }
           }
-          .disabled(!userSettings.followActiveWindow)
         }
         .padding()
         .frame(maxWidth: .infinity, alignment: .leading)
