dfbd297f9f597d42f7cc7b002e2353e41a4ce138
Takayama Fumihiko <tekezo@pqrs.org>
2025-11-22 22:28:55 +0900

    Tweak SettingsMainView.swift

--- src/ShowyEdge/swift/Views/Settings/SettingsMainView.swift
+++ src/ShowyEdge/swift/Views/Settings/SettingsMainView.swift
@@ -7,6 +7,8 @@ struct SettingsMainView: View {
   @ObservedObject private var openAtLogin = OpenAtLogin.shared
   @ObservedObject private var workspaceData = WorkspaceData.shared
 
+  @State private var hoverLanguageColor: LanguageColor?
+
   var body: some View {
     VStack(alignment: .leading, spacing: 25.0) {
       GroupBox(label: Text("Basic")) {
@@ -52,55 +54,89 @@ struct SettingsMainView: View {
       GroupBox(label: Text("Color")) {
         VStack(alignment: .leading, spacing: 10.0) {
           if $userSettings.customizedLanguageColors.count > 0 {
-            ScrollView {
-              Grid(alignment: .leading) {
+            ScrollViewReader { proxy in
+              List {
                 ForEach($userSettings.customizedLanguageColors) { $languageColor in
-                  GridRow {
-                    VStack {
+                  // Make a copy to use it in onHover.
+                  // (Without copy, the program crashes with an incorrect reference when the profile is deleted.)
+                  let languageColorCopy = languageColor
+
+                  VStack(alignment: .leading) {
+                    HStack(alignment: .center, spacing: 4) {
                       Text(
                         workspaceData.getInputSourceLocalizedName(
                           inputSourceID: languageColor.inputSourceID)
                       )
+                      .padding(.trailing, 2)
                       .fixedSize(horizontal: false, vertical: true)
-                      .frame(maxWidth: .infinity, alignment: .leading)
                       .if(languageColor.inputSourceID == workspaceData.currentInputSourceID) {
                         $0.foregroundColor(.accentColor)
                       }
+                      .frame(maxWidth: .infinity, alignment: .leading)
+                      .if(hoverLanguageColor == languageColorCopy) {
+                        $0.overlay(
+                          RoundedRectangle(cornerRadius: 2)
+                            .inset(by: -4)
+                            .stroke(
+                              Color.accentColor,
+                              lineWidth: 2
+                            )
+                        )
+                      }
 
-                      Text(languageColor.inputSourceID)
-                        .font(.caption)
-                        .padding(.leading, 8)
-                        .fixedSize(horizontal: false, vertical: true)
-                        .frame(maxWidth: .infinity, alignment: .leading)
+                      HStack(alignment: .center, spacing: 4) {
+                        ColorPicker("color 1", selection: $languageColor.colors.0)
+                          .labelsHidden()
+
+                        ColorPicker("color 2", selection: $languageColor.colors.1)
+                          .labelsHidden()
+
+                        ColorPicker("color 3", selection: $languageColor.colors.2)
+                          .labelsHidden()
+
+                        Button(
+                          role: .destructive,
+                          action: {
+                            userSettings.removeCustomizedLanguageColor(
+                              languageColor.inputSourceID
+                            )
+                          },
+                          label: {
+                            Label("Delete", systemImage: "trash")
+                              .labelStyle(.iconOnly)
+                              .foregroundColor(.red)
+                          }
+                        )
+                        .padding(.leading, 10)
+                      }
+                      .padding(.leading, 10)
+                      .onHover { hovering in
+                        if hovering {
+                          hoverLanguageColor = languageColorCopy
+                        } else {
+                          if hoverLanguageColor == languageColorCopy {
+                            hoverLanguageColor = nil
+                          }
+                        }
+                      }
                     }
 
-                    ColorPicker("color 1", selection: $languageColor.colors.0)
-                      .labelsHidden()
-
-                    ColorPicker("color 2", selection: $languageColor.colors.1)
-                      .labelsHidden()
-
-                    ColorPicker("color 3", selection: $languageColor.colors.2)
-                      .labelsHidden()
-
-                    Button(
-                      role: .destructive,
-                      action: {
-                        userSettings.removeCustomizedLanguageColor(
-                          languageColor.inputSourceID
-                        )
-                      },
-                      label: {
-                        Label("Delete", systemImage: "trash")
-                          .labelStyle(.iconOnly)
-                          .foregroundColor(.red)
-                      }
-                    )
-                    .padding(.leading, 20)
+                    Text(languageColor.inputSourceID)
+                      .font(.caption)
+                      .fixedSize(horizontal: false, vertical: true)
                   }
                 }
               }
-            }.frame(height: 200)
+              .frame(height: 200)
+              .onChange(of: userSettings.customizedLanguageColors.count) { _ in
+                if let first = userSettings.customizedLanguageColors.first {
+                  withAnimation {
+                    // Reset position when customizedLanguageColors is added.
+                    proxy.scrollTo(first.id, anchor: .top)
+                  }
+                }
+              }
+            }
           } else {
             Text(currentInputSourceLocalizedName)
               .foregroundColor(.gray)
