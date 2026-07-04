6d5d11bee508238f68b8fbde824e9e52f3302c80
Takayama Fumihiko <tekezo@pqrs.org>
2026-07-04 21:12:51 +0900

    Add tools/dump-input-sources

---
+++ tools/dump-input-sources/Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version: 5.5
+
+import PackageDescription
+
+let package = Package(
+  name: "dump-input-sources",
+  targets: [
+    .executableTarget(
+      name: "dump-input-sources"),
+  ]
+)
